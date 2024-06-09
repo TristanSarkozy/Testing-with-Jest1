7c58a5f5e1fb15eb79a49bf988dc8ae607053870
Suciu Dan <suciu_t_dan@yahoo.com>
2024-06-09 16:42:07 +0000

    Add code to addition function so both tests passed

--- script/calc.js
+++ script/calc.js
@@ -1,6 +1,7 @@
 // Add a function to return 42
-function addition() {
-    return 42;
+// Add code to modify the addition function so both tests pass
+function addition(num1, num2) {
+    return num1 + num2;
 }
 
 // Add code to export the function
