516dd5f733ad58c0d0714f052fb0ced9ee4b61df
Suciu Dan <suciu_t_dan@yahoo.com>
2024-06-09 18:57:56 +0000

    Add code to mock the entire html page

---
+++ 03_mocking a complete HTML page/scripts/tests/button.test.js
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// Assign the buttonClick function to a constant
+const buttonClick = require("../button");
+
+// Add Jest directive that runs before each test is run
+beforeEach(() => {
+    // Add the node fs module, a file system handling module
+    // that allows us to open, read and write files
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+// Add a top parent describe block that contains the test and what we expect
+describe("DOM tests", () => {
+    test("expects p content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("YOU Clicked");
+    });
+    // Add a test to see if H1 exists
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    });
+});
